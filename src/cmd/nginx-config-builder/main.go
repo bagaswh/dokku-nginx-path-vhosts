2f9d83aaa860ed3864749c86bb83d79e9fb4fee9
Bagas Wahyu Hidayah <bagaswh00@gmail.com>
2025-10-08 11:24:37 +0700

    updates

--- src/cmd/nginx-config-builder/main.go
+++ src/cmd/nginx-config-builder/main.go
@@ -2,33 +2,26 @@ package main
 
 import (
 	dokkuproperty "dokku-nginx-custom/src/pkg/dokku_property"
-	"flag"
 	"fmt"
+	"log"
 	"os"
-
-	"github.com/dokku/dokku/plugins/common"
 )
 
 var configFilePathPropertyName string = "config-file"
 
 func main() {
 
-	fmt.Println("args=", os.Args)
+	if len(os.Args) < 2 {
+		log.Fatalln("app name (positional arg 2nd) is required")
+	}
 
-	var appName string
-	flag.StringVar(&appName, "app", "", "the app name")
+	appName := os.Args[1]
+
 	if appName == "" {
-		// log.Fatalln("--app flag is required")
+		log.Fatalln("app name (positional arg 2nd) should be non-empty")
 	}
 
 	configFilePath := dokkuproperty.GetAppProperty(appName, configFilePathPropertyName)
-	fmt.Println(configFilePath)
-
-	fmt.Println(
-		"appName=", appName,
-		"nginx=", common.PropertyGet("nginx", "laravel-app", "config-file"),
-		"nginx-custom=", common.PropertyGet("nginx-custom", "laravel-app", "config-file"),
-		"nginx-custom-vhosts=", common.PropertyGet("nginx-custom-vhosts", "laravel-app", "config-file"),
-	)
+	fmt.Println("configFilePath=", configFilePath)
 
 }
